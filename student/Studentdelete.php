ab3bf9f5171659045f8391aecf6fbadffe651149
Brendah <ondiekbrendah@gmail.com>
2018-07-23 16:48:57 -0700

    estatessystem

---
+++ student/Studentdelete.php
@@ -0,0 +1,31 @@
+ <?php
+include "db_Connection.php";
+//Inherit session variables
+session_start();
+
+//Class to connect to database and delete account
+class deleteAccount extends DB_Connect{
+    function __construct(){
+        
+        $this::perform();
+    }
+    //Function to delete account from database
+    function perform(){
+        if(isset($_SESSION['username']) && isset($_SESSION['email'])){
+           
+            //Get variables from session
+            $user = $_SESSION['username'];
+            $email = $_SESSION['email'];
+           
+            $query = "DELETE FROM PROJECT.student where name= ? OR email= ? ;";
+            $pre=$this->connect()->prepare($query);
+	       $pre->execute([$user,$email]);
+            session_destroy();//Close session after deleting account
+            echo  "<script>window.location.href='Studentindex.php';</script>";//Redirect to home page
+        }
+    } 
+}
+
+$delete = new deleteAccount();
+
+?>
