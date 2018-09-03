59e12a680f2225abcc3a0abcefe98345379954c4
Josephine Machage <josephinemachage71@gmail.com>
2018-09-03 15:53:02 +0300

    add

--- student/Studentprofile.php
+++ student/Studentprofile.php
@@ -76,6 +76,50 @@ if(!isset($_SESSION['username'])){
 
 <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
 <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <div id="main" style="margin-left:400px; margin-right: 400px; margin-top: 50px; margin-bottom: 100px;height: 500px; width: 400px;">
+
+        <p style="font-family: Serif;font-size: 30px;margin-left: 38px;">My Profile</p><br>
+        <div style="box-shadow: 5px 5px 5px 5px  #ccccff;margin-right: 10px;background-color: #f3f3f3">
+            <br><br>
+            <form method="POST" action="StudentReset_password.php">
+                <div class="form-group">
+                    <label style="margin-left: 80px;font-family: Serif;font-size: 16px">Username:</label><br>
+                    <div class="col-sm-10">
+                        <input class="form-control"  id="exampleInputUsername" type="textfield" name="username" readonly value="<?php echo  $_SESSION['username']?>">
+
+                    </div>
+                </div><br>
+                <div class="form-group">
+                    <label style="margin-left: 80px;font-family: Serif;font-size: 16px">Email:</label><br>
+                    <div class="col-sm-10">
+                        <input class="form-control"  id="exampleInputUsername" type="textfield" name="email" readonly value="<?php echo  $_SESSION['email']?>">
+
+                    </div>
+                </div><br>
+                <div class="form-group">
+                    <label style="margin-left: 80px;font-family: Serif;font-size: 16px">Worker ID/Registration Number</label><br>
+                    <div class="col-sm-10">
+                        <input class="form-control"  id="exampleInputUsername" type="textfield" name="email" readonly value="<?php echo  $_SESSION['regno']?>">
+
+                    </div>
+                </div><br>
+<!--                <div class="form-group">-->
+<!--                    <label style="margin-left: 80px;font-family: Serif;font-size: 16px">Change Password</label><br>-->
+<!--                    <div class="col-sm-10">-->
+<!--                        <input <input class="btn" style="margin-left: 10px;background-color: #0056b3;color: white; font-family: Serif;font-size: 18px;width: 280px;" type="Submit" name="update" value ="Reset"></br>-->
+<!---->
+<!--                    </div>-->
+<!--                </div><br>-->
+
+
+
+
+<!--                <input class="btn" style="margin-left: 60px;background-color: #0056b3;color: white; font-family: Serif;font-size: 18px;width: 280px;" type="Submit" name="update" value ="Reset"></br>-->
+
+            </form>
+            <br><br>
+        </div>
+    </div>
 
     </body>
 
